69d506525d1a8de50c15dc2cf5ed0a9d2097565a
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-01-16 17:21:16 +0100

    Initial commit.

---
+++ javascript/footer.js
@@ -0,0 +1,17 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+var randomIntEl = document.getElementsByClassName("randomInt");
+
+for(var i = 0; randomIntEl.length > i; i++) {
+    randomIntEl[i].innerText = getRandomInt(3, 18);
+}
+
+var armorEl = document.getElementsByClassName("armor")[0];
+var mwlEl = document.getElementsByClassName("mwl")[0];
+
+armorEl.innerHTML = getRandomInt(1, 8);
+mwlEl.innerHTML = parseInt(randomIntEl[1].innerHTML)/2;
