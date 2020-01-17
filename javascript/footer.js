7a8963ebe76466da6166c98adca94968f8f1d7a2
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-01-17 09:34:10 +0100

    Added upload and download function.

--- javascript/footer.js
+++ javascript/footer.js
@@ -17,5 +17,26 @@ armorEl.innerHTML = getRandomInt(1, 8);
 mwlEl.innerHTML = parseInt(randomIntEl[1].innerHTML)/2;
 
 function saveChar() {
+    var formData = new FormData(document.getElementsByClassName("userForm")[0]);
+    var object = {};
+    formData.forEach(function(value, key){
+        object[key] = value;
+    });
+    var json = JSON.stringify(object);
+    console.log(json);
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(json);
+    var dlAnchorElem = document.getElementById('downloadAnchorElem');
+    dlAnchorElem.setAttribute("href", dataStr);
+    dlAnchorElem.setAttribute("download", "scene.json");
+    dlAnchorElem.click();
+}
 
+function loadChar() {
+    var importedJson = document.getElementsByClassName("load")[0].files;
+    var fr = new FileReader();
+    fr.onload = function(e) {
+        var result = JSON.parse(e.target.result);
+        console.log(result);
+    }
+    fr.readAsText(importedJson.item(0));
 }
