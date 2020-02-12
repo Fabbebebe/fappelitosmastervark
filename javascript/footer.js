1b39ed58aa2cc3ff89b1ad84b82b60d29a80ff59
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-02-12 20:14:29 +0100

    Added comments.

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,28 +1,32 @@
+//Random integer generator for the character
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+//Setting some global variables for use later in loops
 var randomIntEl = document.getElementsByClassName("randomInt");
 var userForm = document.getElementsByClassName("userForm")[0];
 var userFormInputs = userForm.getElementsByTagName("input");
+var armorEl = document.getElementsByClassName("armor")[0];
+var mwlEl = document.getElementsByClassName("mwl")[0];
 
 
 function clearAtt() {
     document.getElementsByName("name")[0].previousElementSibling.removeAttribute("style");
 }
 
+//Asigns a random integer to each of the fields that require random ints
 for(var i = 0; randomIntEl.length > i; i++) {
     randomIntEl[i].innerText = getRandomInt(3, 18);
 }
 
-var armorEl = document.getElementsByClassName("armor")[0];
-var mwlEl = document.getElementsByClassName("mwl")[0];
-
 armorEl.innerHTML = getRandomInt(1, 8);
 mwlEl.innerHTML = parseInt(randomIntEl[1].innerHTML)/2;
 
+//Creates a JSON object with all the data
+//The JSON object is parsed bit for bit and turned into a downloadable file that the user then gets a prompt asking where to save the file
 function saveChar() {
     if(document.getElementsByName("name")[0].value !== "") {
         var formData = new FormData(userForm);
@@ -49,6 +53,7 @@ function saveChar() {
     }
 }
 
+//When the user clicks on "import", it parses the file content and converts the JSON object into an array of which then content is fed into the fields
 function loadChar() {
     var importedJson = document.getElementsByClassName("load")[0].files;
     var fr = new FileReader();
